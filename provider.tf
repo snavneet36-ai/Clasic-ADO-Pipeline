70dc17b17622cf9b3267d96d3b616f4373b5919e
NAVNEET SINGH <snavneet36@gmail.com>
2026-08-21 09:56:17 +0530

    Update Terraform provider configuration

--- provider.tf
+++ provider.tf
@@ -2,11 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "5.2.0"
+      version = "=5.0.0"
     }
   }
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  # Configuration options
+  features {}
 }
