7af95dfeba13aa9ff501be0e7976aba6119df743
snavneet36-ai <snavneet36@gmail.com>
2026-08-21 10:30:33 +0530

    Add a new Azure resource group 'example1'

--- resources.tf
+++ resources.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "example" {
   name     = "Navneet_sherlock"
   location = "West Europe"
 }
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "example1" {
   name     = "Navneet_sherlock1"
   location = "West Europe"
 }
